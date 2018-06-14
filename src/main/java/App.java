5ca8d21c3e48d5d7570f04c46183a357fe6ffc54
hbosch <hboschcap@gmail.com>
2018-06-14 14:02:17 +0200

    Initiate Development Branch

---
+++ src/main/java/App.java
@@ -0,0 +1,54 @@
+import com.hotelmolveno.user.Guest;
+
+/**
+ * Molveno World Program!
+ *
+ */
+
+
+public class App {
+    String MessageFromMain = "message from main";
+
+
+    public static void main(String[] args) {
+        System.out.println(Room);
+    }
+
+
+    public String getMessageFromMain() {
+        return MessageFromMain;
+    }
+
+    public void setMessageFromMain(String messageFromMain) {
+        MessageFromMain = messageFromMain;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+public class RoomStore implements Iterable<Room>{
+    private Map<Interget, Guest guest = new HashMap<>();
+}
+
+Scanner reader  - new Scanner (System.in);
+
+reader.close();
+return choice;
+
+privte Map<Integer, Room> rooms = new HashMap<>();
+*/
