41ce00b2f789f3263c29cd5d808b3933213759f0
hbosch <hboschcap@gmail.com>
2018-06-14 15:01:19 +0200

    Adjusted main app

---
+++ src/main/java/com/hotelmolveno/App.java
@@ -0,0 +1,12 @@
+package com.hotelmolveno;
+
+import com.hotelmolveno.hotel.Room;
+
+public class App{
+
+    public static void main(String[]args){
+    Room newRoom = new Room();
+
+    System.out.println(newRoom.getRoomID());
+        }
+}
