b0fc70e8623b9c607b5d9fc13f08cbc773de3faa
Eva van Rooden <evavanrooden@gmail.com>
2018-06-20 15:16:27 +0200

    added user input to roommakerapp

--- src/main/java/com/hotelmolveno/hotel/RoomMakerApp.java
+++ src/main/java/com/hotelmolveno/hotel/RoomMakerApp.java
@@ -1,8 +1,11 @@
 package com.hotelmolveno.hotel;
+
 import java.util.Scanner;
 
 
 public class RoomMakerApp {
+
+    boolean state = true;
 
     public static void main(String[] args) {
         printMenu();
@@ -23,13 +26,13 @@ public class RoomMakerApp {
             System.out.println("Enter a number: ");
             int x = reader.nextInt();
 //once finished
-            reader.close();
+            // reader.close();
             System.out.println("User input was " + Integer.toString(x));
 
             switch (x) {
                 case 1:
                     System.out.println("You choose to make a room");
-                    roomReservation();
+                    roomMaker();
                     break; //
                 case 2:
                     System.out.println("You choose to alter a room");
@@ -44,12 +47,18 @@ public class RoomMakerApp {
             }
 
         } catch (Exception e) {  // if user doesn't enter integer, do this:
-            System.out.println("Please choose 1, 2 or 3");
+            System.out.println("Please choose a number");
         }
 
     }
 
-    public static void roomReservation() { // method to run if guest chooses to reserve a room
+    public static void roomMaker() { // method to run if employee chooses to make a room
+            Scanner reader = new Scanner(System.in);  // Reading from System.in
+            System.out.println("Enter a room number: ");
+            int x = reader.nextInt();
+//once finished
+            reader.close();
+            System.out.println("You entered room number " + Integer.toString(x));
 
     }
 }
