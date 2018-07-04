2e2d1dbb5a07e94705518786d7433b311bf5b3fc
Eva van Rooden <evavanrooden@gmail.com>
2018-07-04 14:49:19 +0200

    working CRUD

--- src/main/resources/static/assets/scripts/test.js
+++ src/main/resources/static/assets/scripts/test.js
@@ -30,7 +30,7 @@ function showOurRooms() {
 }
 
 function createRoomString(room) {
- result = "<table><tr><td>id"+room.roomID+"</td><td>number"+room.roomNumber+"</td><td>guests"+room.numberOfGuests+"</td><td>price"+room.price+"</td><td>reserved"+room.reserved+"</td></tr></table>";
+ result = "<table><tr><td>id "+room.roomID+"</td><td>number "+room.roomNumber+"</td><td>guests "+room.numberOfGuests+"</td><td>price "+room.price+"</td><td>reserved "+room.reserved+"</td></tr></table>";
 
   return result;
 }
@@ -41,22 +41,6 @@ $("#addButton").click(function() {
                 roomNumber: $("#roomNumber").val(),
                 numberOfGuests: $("#numberOfGuests").val(),
                 price: Number($("#price").val())
-//                reserved: $("#reserved").val(),
-//                reserved: $("#checkbox1").is(':checked', function(){
-//                            $("#checkbox1").prop('checked', true);
-//                          });
-                reserved: $('#checkbox-value').text($('#checkbox1').val());
-
-                          $("#checkbox1").on('change', function() {
-                            if ($(this).is(':checked')) {
-                              $(this).attr('value', 'true');
-                            } else {
-                              $(this).attr('value', 'false');
-                            }
-
-                            $('#checkbox-value').text($('#checkbox1').val());
-                          });
-
 
             };
              $.ajax({
@@ -67,29 +51,48 @@ $("#addButton").click(function() {
                      type : "post",
                      data: JSON.stringify(jsonObject),
                      // als de actie lukt, voer deze functie uit
-                     success: function(data){ // so the data is the bulb of the response of the Spring Boot REST controller
 
-                            $("#newId").html(data.id);
-                            $("#newFirstName").html(data.roomNumber);
-                            $("#newLastName").html(data.numberOfGuests);
-                            $("#newLastName").html(data.price);
-//                            $("#newLastName").html(data.reserved);
-                            $("#newLastName").html(data.reserved);
-
-
-
-                            $("#addedPerson").show(2000);
-
-                            $("#addedPerson").hide(2000);
-
-                          console.log(data);
-                     }
                  });
           });
 
 
+$("#deleteButton").click(function() {
+            var roomID = Number($("#roomToDelete").val())
+
+             $.ajax({
+                    contentType : "application/json",
+                     // waar moet hij de request op uitvoeren
+                     url : baseUrl+"/rooms/" + roomID,
+                     // type actie
+                     type : "delete",
+
+                     // als de actie lukt, voer deze functie uit
+
+                 });
+          });
 
 
+$("#updateButton").click(function() {
+
+            var roomID = Number($("#roomToUpdate").val())
+
+            var jsonObject = {
+                roomNumber: $("#updateRoomNumber").val(),
+                numberOfGuests: $("#updateNumberOfGuests").val(),
+                price: Number($("#updatePrice").val())
+
+            };
+             $.ajax({
+                    contentType : "application/json",
+                     // waar moet hij de request op uitvoeren
+                     url : baseUrl+"/rooms/" + roomID,
+                     // type actie
+                     type : "put",
+                     data: JSON.stringify(jsonObject),
+                     // als de actie lukt, voer deze functie uit
+
+                 });
+          });
 
 
 
