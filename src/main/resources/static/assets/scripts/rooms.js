fdd1b6a5177015d9f9446bf98f51601a307ca7a5
Eva van Rooden <evavanrooden@gmail.com>
2018-07-05 14:16:42 +0200

    working modal :D

--- src/main/resources/static/assets/scripts/rooms.js
+++ src/main/resources/static/assets/scripts/rooms.js
@@ -19,20 +19,20 @@ $(document).ready(function() {
          } );
 
 
-//    // Functionality for interaction when clicking on rows of the table
-//        $('#dataTable tbody').on( 'click', 'tr', function () {
-//            if ( $(this).hasClass('selected') ) {
-//                $(this).removeClass('selected');
-//            }
-//            else {
-//                deselect();
-//                $(this).addClass('selected');
-//                var table = $('#dataTable').DataTable();
-//                var data = table.row(this).data();
-//                apiGetSingleGuest(data.id);
-//                $('#myModal').modal('toggle');
-//            }
-//        });
+    // Functionality for interaction when clicking on rows of the table
+        $('#dataTable tbody').on( 'click', 'tr', function () {
+            if ( $(this).hasClass('selected') ) {
+                $(this).removeClass('selected');
+            }
+            else {
+                deselect();
+                $(this).addClass('selected');
+                var table = $('#dataTable').DataTable();
+                var data = table.row(this).data();
+                apiGetSingleRoom(data.roomID);
+                $('#myModal').modal('toggle');
+            }
+        });
 
 } );
 
@@ -98,3 +98,101 @@ $("#addButton").click(function() {
                                      });
                                      location.reload();
                               });
+
+     function apiGetSingleRoom(id){
+         var api = baseUrl + "/" + id;
+         $.get(api, function(data){
+             if (data){
+                 fillUpdateDiv(data);
+             }
+         });
+     }
+
+function fillUpdateDiv(room){
+
+    console.log(room);
+    $("#btndelete").attr('onclick', 'submitDelete(' + room.roomID + ');');
+    $("#btnsubmit").attr('onclick', 'submitEdit(' + room.roomID + ');');
+    document.getElementById("modal-title").innerHTML="Edit Room";
+    $("#roomNumber").val(room.roomNumber);
+    $("#numberOfGuests").val(room.numberOfGuests);
+    $("#price").val(room.price);
+     /*
+    $("#postalCode").val(room.postalCode);
+    $("#city").val(room.city);
+    $("#country").val(room.country);
+    $("#phoneNumber").val(room.phoneNumber);
+    $("#mailAddress").val(room.mailAddress);
+    */
+    $("#confirmbutton").css('display', 'inline-block');
+    deleteID = room.roomID;
+    var elem = '<button type="button" class="btn btn-danger" onclick="submitDelete();">Confirm delete</button>';
+    $('#confirmbutton').popover({
+        animation:true,
+        content:elem,
+        html:true,
+        container: myModal
+    });
+}
+
+function deselect(){
+    $('#dataTable tr.selected').removeClass('selected');
+    // rloman dit moet straks terug. ik denk dat dit het modal form is
+    document.getElementById("roomForm").reset();
+}
+
+
+function submitEdit(id){
+// shortcut for filling the formData as a JavaScript object with the fields in the form
+    console.log("Formdata");
+    var formData = $("#roomForm").serializeArray().reduce(function(result, object){ result[object.name] = object.value; return result}, {});
+    console.log(formData);
+
+    for(var key in formData){
+        if(formData[key] == "" || formData == null) delete formData[key];
+    }
+    $.ajax({
+        url:baseUrl +"/" + id,
+        type:"put",
+        data: JSON.stringify(formData),
+        contentType: "application/json; charset=utf-8",
+        success: getData,
+        error: function(error){
+            displayError(error);
+        }
+    });
+    deselect();
+    $('#myModal').modal('toggle');
+}
+
+
+function getData() {
+      var api = baseUrl;
+        $.get(api, function(data){
+            if (data){
+                setData(data);
+            }
+        });
+}
+
+function setData(data){
+    $("#dataTable").DataTable().clear();
+    $("#dataTable").DataTable().rows.add(data);
+    $("#dataTable").DataTable().columns.adjust().draw();
+}
+
+function submitDelete(){
+    console.log("Deleting");
+    var formData = $("#roomForm").serializeArray().reduce(function(result, object){ result[object.name] = object.value; return result}, {});
+    var guestNumber = deleteID;
+    $.ajax({
+        url:baseUrl + "/" + guestNumber,
+        type:"delete",
+        data: JSON.stringify(formData),
+        success: getData,
+        contentType: "application/json; charset=utf-8"
+    });
+
+    $('#myModal').modal('toggle');
+    deselect();
+}
