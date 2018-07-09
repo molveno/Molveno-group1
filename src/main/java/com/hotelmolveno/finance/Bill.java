738aacd82d278696155c99da6e3ced1018d3d4a3
sjannekeeggermont <sjanneke.eggermont@capgemini.com>
2018-07-09 14:12:10 +0200

    nog meer relaties

--- src/main/java/com/hotelmolveno/finance/Bill.java
+++ src/main/java/com/hotelmolveno/finance/Bill.java
@@ -1,5 +1,7 @@
 package com.hotelmolveno.finance;
 
+import com.hotelmolveno.user.Guest;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,25 +20,30 @@ public class Bill {
     private double totalCost;
     private double remainingAmount;
 
-    @OneToMany (mappedBy = "bill")
-    private Set<Payment> paymentsDone = new HashSet<>();
+    @ManyToOne
+    private Guest guest;
+
+    @OneToMany(mappedBy = "person") // person is the private Person instance var in the phone class
+    private Set<Payment> payments = new HashSet<>();
+
+    public Set<Payment> getPayment() {
+        return this.payments;
+    }
+
+    public void addPayment(Payment payment) {
+        this.payments.add(payment);
+
+    }
 
     public Bill() {
-    }
+        }
 
-    public Set<Payment> getPaymentsDone() {
-        return paymentsDone;
-    }
 
-    public void setPaymentsDone(Set<Payment> paymentsDone) {
-        this.paymentsDone = paymentsDone;
-    }
-
-    public double calculateTotalCost() {
-        totalCost = roomCost + restaurantCost + barCost + otherCost;
-        System.out.println("The total cost is " + totalCost);
-        return totalCost;
-    }
+        public double calculateTotalCost () {
+            totalCost = roomCost + restaurantCost + barCost + otherCost;
+            System.out.println("The total cost is " + totalCost);
+            return totalCost;
+        }
 
 //    public double calculateRemainingAmount() {
 //        remainingAmount = totalCost - paymentDone.getPaymentAmount();
@@ -44,61 +51,61 @@ public class Bill {
 //    }
 
 
-    public long getId() {
-        return id;
+        public long getId () {
+            return id;
+        }
+
+        public void setId ( long id){
+            this.id = id;
+        }
+
+        public double getRoomCost () {
+            return roomCost;
+        }
+
+        public void setRoomCost ( double roomCost){
+            this.roomCost = roomCost;
+        }
+
+        public double getRestaurantCost () {
+            return restaurantCost;
+        }
+
+        public void setRestaurantCost ( double restaurantCost){
+            this.restaurantCost = restaurantCost;
+        }
+
+        public double getBarCost () {
+            return barCost;
+        }
+
+        public void setBarCost ( double barCost){
+            this.barCost = barCost;
+        }
+
+        public double getOtherCost () {
+            return otherCost;
+        }
+
+        public void setOtherCost ( double otherCost){
+            this.otherCost = otherCost;
+        }
+
+        public double getTotalCost () {
+            return totalCost;
+        }
+
+        public void setTotalCost ( double totalCost){
+            this.totalCost = totalCost;
+        }
+
+        public double getRemainingAmount () {
+            return remainingAmount;
+        }
+
+        public void setRemainingAmount ( double remainingAmount){
+            this.remainingAmount = remainingAmount;
+        }
+
+
     }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public double getRoomCost() {
-        return roomCost;
-    }
-
-    public void setRoomCost(double roomCost) {
-        this.roomCost = roomCost;
-    }
-
-    public double getRestaurantCost() {
-        return restaurantCost;
-    }
-
-    public void setRestaurantCost(double restaurantCost) {
-        this.restaurantCost = restaurantCost;
-    }
-
-    public double getBarCost() {
-        return barCost;
-    }
-
-    public void setBarCost(double barCost) {
-        this.barCost = barCost;
-    }
-
-    public double getOtherCost() {
-        return otherCost;
-    }
-
-    public void setOtherCost(double otherCost) {
-        this.otherCost = otherCost;
-    }
-
-    public double getTotalCost() {
-        return totalCost;
-    }
-
-    public void setTotalCost(double totalCost) {
-        this.totalCost = totalCost;
-    }
-
-    public double getRemainingAmount() {
-        return remainingAmount;
-    }
-
-    public void setRemainingAmount(double remainingAmount) {
-        this.remainingAmount = remainingAmount;
-    }
-
-
-}
